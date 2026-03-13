ceada57626c3fe54159944e03fc7e8f918ae62d9
Arjun_2025 <vineetaarjun123@gmail.com>
2026-03-13 13:34:45 +0530

    added gsap folder 13-03-26

---
+++ gsap/1_intro/main.js
@@ -0,0 +1,8 @@
+//gsap code
+
+gsap.to("h1",{
+    duration : 6,
+    opacity : 1,
+    y : -60,
+    ease : "power3.out"
+})
---
+++ gsap/2_basics/main.js
@@ -0,0 +1,35 @@
+// BASICS
+//gsap.to()
+//gsap.from()
+//gsp.fromto()
+
+gsap.fromTo(".box1",{
+     x: 190,
+    duration : 1,
+    opacity : 1
+},{
+    x: -300,
+    duration : 6,
+    delay: 1,
+    opacity : 0 
+})
+
+
+gsap.fromTo(".box2",{
+    x : -200,
+    opacity : 1,
+    duration : 1
+},{
+    x: 300,
+    duration : 6,
+    delay : 1,
+    opacity : 0
+})
+
+gsap.to("h1",{
+    y:-80,
+    duration : 3,
+    delay : 2,
+    opacity : 1
+    
+})
